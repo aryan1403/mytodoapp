f2309e1fb3c46baf0d9e0476e2c905bfdeaf098c
Aaryan <aaryan14032006@gmail.com>
2021-11-11 11:57:09 +0530

    udpated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import myComp from './myComp';
 
 function App() {
+  let name1 = "Aaryan";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello {name1}</h1>
+      <myComp name="Arush"/>
     </div>
   );
 }
---
+++ src/myComp.js
@@ -0,0 +1,8 @@
+import './myComp.css';
+
+
+function myComp() {
+    return <h2>Hi, I am a Car!</h2>;
+}
+
+export default myComp;
